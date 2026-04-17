19813ed179c871af82101db987fd1ee78dd0f323
Mohammed Malla <mohamedsamirs732@gmail.com>
2026-04-17 15:04:55 +0200

    Regiter

--- src/ExaminationSystem.Application/Interfaces/IQuery.cs
+++ src/ExaminationSystem.Application/Interfaces/IQuery.cs
@@ -7,6 +7,8 @@ namespace ExaminationSystem.Application.Interfaces
     public interface IQuery : IRequest
     {
     }
+
+    ///
     public interface IQuery<TResponse> : IRequest<TResponse>
     {
     }
