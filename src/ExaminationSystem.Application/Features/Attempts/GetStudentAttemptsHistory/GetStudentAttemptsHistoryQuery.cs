cbe66822583dd690b91e7824596f16f09d61501d
karim salah <karimabdelghany753@gmail.com>
2026-04-25 04:36:41 -0700

    upadate Api respose Consistent JSON Response Envelope - return meta as pagniated data in function success - refactor GetPublishedDiplomaQuizezQuery with ICurrent service and refactor GetStudentAttemptsHistoryQuery to take pagniation params as requet parmeter

--- src/ExaminationSystem.Application/Features/Attempts/GetStudentAttemptsHistory/GetStudentAttemptsHistoryQuery.cs
+++ src/ExaminationSystem.Application/Features/Attempts/GetStudentAttemptsHistory/GetStudentAttemptsHistoryQuery.cs
@@ -5,19 +5,19 @@ using LinqKit;
 namespace ExaminationSystem.Application.Features.Attempts.GetStudentAttemptsHistory;
 
 public record GetStudentAttemptsHistoryQuery
-(Guid? QuizId , Guid? DiplomaId , int? PageNumber , int? PageSize , Guid StudentId) 
-: IQuery<RequestResult<PaginationResult<GetStudentAttemptHistoryResponse>>>;
+(Guid? QuizId , Guid? DiplomaId , PaginationParams Params , Guid StudentId) 
+: IQuery<RequestResult<PaginatedResult<GetStudentAttemptHistoryResponse>>>;
 
 public class GetStudentAttemptsHistoryQueryHandler(IRepository<QuizAttempt> _repository
             ,ILogger<GetStudentAttemptsHistoryQueryHandler> _logger)
-    : IRequestHandler<GetStudentAttemptsHistoryQuery, RequestResult<PaginationResult<GetStudentAttemptHistoryResponse>>>
+    : IRequestHandler<GetStudentAttemptsHistoryQuery, RequestResult<PaginatedResult<GetStudentAttemptHistoryResponse>>>
 {
 
-    public async Task<RequestResult<PaginationResult<GetStudentAttemptHistoryResponse>>> Handle(GetStudentAttemptsHistoryQuery request, CancellationToken cancellationToken)
+    public async Task<RequestResult<PaginatedResult<GetStudentAttemptHistoryResponse>>> Handle(GetStudentAttemptsHistoryQuery request, CancellationToken cancellationToken)
     {
         _logger.LogInformation(
           "Fetching attempt history for Student {StudentId} | DiplomaId: {DiplomaId}, QuizId: {QuizId}, Page: {Page}/{Size}",
-          request.StudentId, request.DiplomaId, request.QuizId, request.PageNumber, request.PageSize);
+          request.StudentId, request.DiplomaId, request.QuizId, request.Params.Page, request.Params.PerPage);
 
         var query = _repository.GetAll(qa => qa.UserId == request.StudentId)
                                .Where (qa =>  !request.QuizId.HasValue  || qa.QuizId == request.QuizId.Value)
@@ -32,10 +32,10 @@ public class GetStudentAttemptsHistoryQueryHandler(IRepository<QuizAttempt> _rep
                                           qa.SubmittedAt
                                       )
                                );
-        var result = await query.PaginateAsync(request.PageNumber , request.PageSize ,cancellationToken);
+        var result = await query.ToPagedAsync(request.Params,cancellationToken);
         _logger.LogInformation(
                "Attempt history returned for Student {StudentId}. TotalCount: {TotalCount}, Page: {Page}/{TotalPages}",
-               request.StudentId, result.TotalCount, result.PageNumber, result.TotalPages);
-        return RequestResult<PaginationResult<GetStudentAttemptHistoryResponse>>.succeeded(result ,ResultCode.QuizAttemptHistoryRetrievedSuccessfully);
+               request.StudentId, result.Total, result.Page, result.TotalPages);
+        return RequestResult<PaginatedResult<GetStudentAttemptHistoryResponse>>.succeeded(result ,ResultCode.QuizAttemptHistoryRetrievedSuccessfully);
     }
 }
